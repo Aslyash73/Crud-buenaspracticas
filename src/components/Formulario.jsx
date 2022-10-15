81f6177b53c7d7c2c253969ae33343f74288ae21
Aslyash73 <acantill37@cuc.edu.co>
2022-10-15 16:39:55 -0500

    configuracion 15-10

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,77 +1,171 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react' //el useEffect genera la vizualizacion del estado
+import { db } from '../firebase';
+import { collection, addDoc, deleteDoc, doc, updateDoc, onSnapshot } from 'firebase/firestore';
 
 const Formulario = () => {
     const [fruta, setFruta] = useState('')
-    const [descripción, setDescripcion] = useState('')
-    const [listaFrutas, setlistaFrutas]=useState([])
-    
-    return (
-        
-        
+    const [descripcion, setDescripcion] = useState('')
+    const [listaFrutas, setListaFrutas] = useState([])
+    const [modoEdicion, setModoEdicion] = useState(false)
+    const [id, setId] = useState('')
 
+
+    useEffect(() => {
+        const obtenerDatos = async () => {
+            try {
+                await onSnapshot(collection(db, "frutas"), (query) => {
+                    setListaFrutas(query.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        obtenerDatos();
+    }, [])
+
+    const eliminar = async id => {
+        try {
+            await deleteDoc(doc(db, 'frutas', id))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    const guardarFrutas = async (e) => {
+        e.preventDefault()
+        try {
+            const data = await addDoc(collection(db, 'frutas'), {
+                nombreFruta: fruta,
+                nombreDescripcion: descripcion
+            })
+            setListaFrutas([
+                ...listaFrutas,
+                { nombreFruta: fruta, nombreDescripcion: descripcion, id: data.id }
+            ])
+
+            setFruta('')
+            setDescripcion('')
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const editarFrutas = async (e) => {
+        e.preventDefault()
+        try{
+            const docRef = doc(db, 'frutas', id);
+            await updateDoc(docRef, {
+                nombreFruta:fruta,
+                nombreDescripcion:descripcion
+            })
+
+            const nuevoArray = listaFrutas.map(
+                item => item.id === id ? {id: id, nombreFruta:fruta, nombreDescripcion:descripcion} : item
+            )
+            
+            setListaFrutas(nuevoArray)
+            setFruta('')
+            setDescripcion('')
+            setId('')
+            setModoEdicion(false)
+
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+
+
+
+
+
+    const editar = item => {
+        setFruta(item.nombreFruta)
+        setDescripcion(item.nombreDescripcion)
+        setId(item.id)
+        setModoEdicion(true)
+    }
+
+    const cancelar = () => {
+        setModoEdicion(false)
+        setFruta('')
+        setDescripcion('')
+        setId('')
+    }
+
+
+
+    return (
         <div className='container mt-5'>
             <h1 className="text-center">CRUD BÁSCIO BUENAS PRÁCTICAS</h1>
             <hr />
-            <div className='row'>
+            < div className='row'>
                 <div className='col-8'>
                     <h4 className='text-center'>Listado de frutas</h4>
                     <ul className='list-group'>
                         {
-                            //listar las personas
+                            listaFrutas.map(item => (
+                                <li className="list-group-item" key={item.id}>
+                                    <span className="lead">{item.nombreFruta}-{item.nombreDescripcion}</span>
+                                    <button className="btn btn-danger btn-sm float-end mx-2"
+                                        onClick={() => eliminar(item.id)}>Eliminar</button>
+                                    <button className="btn btn-warning btn-sm float-end"
+                                        onClick={() => editar(item)}>Editar</button>
+
+                                </li>
+                            ))
                         }
                     </ul>
                 </div>
 
+
                 <div className='col-4'>
                     <h4 className='text-center'>
-                        Agregar Frutas
+                        {
+                            modoEdicion ? 'Editar Frutas' : 'Agregar Frutas'
+                        }
                     </h4>
-                    <form>
+                    <form onSubmit={modoEdicion ? editarFrutas : guardarFrutas}>
                         <input type="text"
-                            className='form-control mb-2'
+                            className="form-control mb-2"
                             placeholder='Ingrese Fruta'
-                            
-                        />
+                            value={fruta}
+                            onChange={(e) => setFruta(e.target.value)} />
                         <input type="text"
-                            className='form-control mb-2'
+                            className="form-control mb-2"
                             placeholder='Ingrese Descripción'
-                        />
-                    
-                        <input type="text"
-                            className='form-control mb-2'
-                            placeholder='Ingrese su Nombre'
-                        />
-                        <input type="text"
-                            className='form-control mb-2'
-                            placeholder='Ingrese su identificacion'
-                        />
-                        <input type="text"
-                            className='form-control mb-2'
-                            placeholder='Ingrese cuantas quiere comprar'
-                        />
-                        <input type="text"
-                            className='form-control mb-2'
-                            placeholder='Ingrese su tienda'
-                        />
-                        <input type="text"
-                            className='form-control mb-2'
-                            placeholder='ingrese año de nacimiento'
-                        />
-                        <button className='btn btn-primary btn-block' type='submit'>
-                            Agregar
-                        </button>
+                            value={descripcion}
+                            onChange={(e) => setDescripcion(e.target.value)} />
 
-                     
+                        {
+                            modoEdicion ?
+                                (
+                                    <>
+                                        <button
+                                            className='btn btn-warning btn-block'
+                                            on='submit'>Editar</button>
+                                        <button
+                                            className='btn btn-dark btn-block mx-2'
+                                            onClick={() => cancelar()}>Cancelar</button>
+                                    </>
+                                )
+                                :
 
-
+                                <button
+                                    type='submit'
+                                    className='btn btn-primary btn-block' >
+                                    agregar
+                                </button>
+                        }
                     </form>
-                   
-                </div>
 
-               
+                </div>
             </div>
         </div>
+
+
     )
 }
-
 export default Formulario
--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'fireba/firestore';
+import {getFirestore} from 'firebase/firestore';
 // import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD7qZvMg1g2XzKIpqeAsdvY193H5eWiFZE",
-  authDomain: "crudbpracticasw.firebaseapp.com",
-  projectId: "crudbpracticasw",
-  storageBucket: "crudbpracticasw.appspot.com",
-  messagingSenderId: "984126797974",
-  appId: "1:984126797974:web:5679d4f762762419382ab7",
-  measurementId: "G-XS0WWEF2VW"
+  apiKey: "AIzaSyCAr8uz9RUk-1Hy4gWKoc2bYUQvsr6Gjqc",
+  authDomain: "crudwebb.firebaseapp.com",
+  projectId: "crudwebb",
+  storageBucket: "crudwebb.appspot.com",
+  messagingSenderId: "377644526650",
+  appId: "1:377644526650:web:f64cfcfdd61c0aab43d903"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 export {db}
+
